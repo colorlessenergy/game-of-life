1c6f7460b6e8ffefd68316afae4cb8d926555a6f
colorlessenergy <brianmunozdev@gmail.com>
2024-02-23 11:53:10 -0800

    add functionality to add a pixel on the grid

--- app/_components/Canvas.js
+++ app/_components/Canvas.js
@@ -2,6 +2,8 @@
 
 import { useEffect, useRef, useState } from 'react';
 
+const PIXEL_SIZE = 40;
+
 const Canvas = () => {
     const canvasRef = useRef(null);
     useEffect(() => {
@@ -13,18 +15,28 @@ const Canvas = () => {
     const [grid, setGrid] = useState([]);
 
     useEffect(() => {
-        for (let i = 0; i < canvasRef.current.width; i += 20) {
-            for (let j = 0; j < canvasRef.current.height; j += 20) {
+        for (let i = 0; i < canvasRef.current.width; i += PIXEL_SIZE) {
+            for (let j = 0; j < canvasRef.current.height; j += PIXEL_SIZE) {
                 setGrid(previousGrid => {
                     previousGrid.push([i, j]);
                     return previousGrid;
                 });
 
                 const context = canvasRef.current.getContext('2d');
-                context.strokeStyle = '#4B3B40';
-                context.strokeRect(i, j, 20, 20);
+                context.strokeStyle = '#FFFFFF';
+                context.strokeRect(i, j, PIXEL_SIZE, PIXEL_SIZE);
             }
         }
+    }, []);
+
+    useEffect(() => {
+        canvasRef.current.addEventListener('click', event => {
+            const context = canvasRef.current.getContext('2d');
+            const pixelX = Math.floor(event.offsetX / PIXEL_SIZE) * PIXEL_SIZE;
+            const pixelY = Math.floor(event.offsetY / PIXEL_SIZE) * PIXEL_SIZE;
+            context.fillStyle = '#FFFFFF';
+            context.fillRect(pixelX, pixelY, PIXEL_SIZE, PIXEL_SIZE);
+        });
     }, []);
 
     return <canvas ref={canvasRef}></canvas>;
